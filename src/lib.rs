0a17fd0c407afb673d53fa9b3380519625979adf
Gaetan Semet <gaetan@xeberon.net>
2013-10-30 12:01:04 +0100

    inital commit: makefile squeleton
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#[link(name = "urlparse",
+       vers = "0.1-pre",
+       uuid = "11edce46-6e7c-46f8-9bba-586d7b1a3581",
+       url = "")];
+
+#[comment = "URL Parser"];
+#[license = "Apache License"];
+#[crate_type = "lib"];
+
